8493775675008b37a0d0534d382ae941a8f58b1e
Kara Downing <karadowning341@gmail.com>
2025-09-24 23:06:01 +0100

    Time: 401 ms (82.63%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,10 +1,5 @@
-SELECT 
-    w1.id
-FROM 
-    Weather w1
-JOIN 
-    Weather w2
-ON 
-    DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE 
-    w1.temperature > w2.temperature;
+SELECT a.id
+FROM Weather a
+JOIN Weather b
+ON a.recordDate = b.recordDate + INTERVAL 1 DAY
+WHERE a.temperature > b.temperature;
